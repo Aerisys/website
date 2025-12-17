b159964e363a6fc663d2cf950203ac103e9f9d3d
gaetanccg <gaetan6942@gmail.com>
2025-12-17 09:28:27 +0100

    initial refacto with updated npm

--- src/config.ts
+++
@@ -1,1 +0,0 @@
-export const CDN_URL = "https://cdn.aerisys.fr/medias/";
--- src/views/NotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="text-center p-52">
-    <h1 class="text-4xl font-bold text-red-500">404</h1>
-    <p class="text-lg text-white">La page que vous recherchez n'existe pas.</p>
-    <router-link to="/" class="text-blue-500 hover:underline">Retour à l'accueil</router-link>
-  </div>
-</template>
