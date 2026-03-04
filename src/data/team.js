0afcc20fc58c7766bb64e9c56e21ade9fdeb5c28
ThibaultR <94461612+Zombizz97@users.noreply.github.com>
2026-03-04 15:52:31 +0100

    change team content + add github worklows (#50)

--- src/data/team.js
+++ src/data/team.js
@@ -1,146 +1,117 @@
 export const teamMembers = [
   {
     id: 1,
-    name: 'John Doe',
-    role: 'Co-founder',
+    name: 'Loan FRANCOIS',
+    role: 'Chef de projet',
     image: '/images/team/member-1.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/loanfrancois/',
+      github: 'https://github.com/LoanF/',
     }
   },
   {
     id: 2,
-    name: 'John Doe',
-    role: 'Co-founder',
+    name: 'Gaël PIDOUX',
+    role: 'Chef de projet',
     image: '/images/team/member-2.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      github: 'https://github.com/gaelpidoux',
     }
   },
   {
     id: 3,
-    name: 'John Doe',
-    role: 'Co-founder',
+    name: 'Petru OLTEAN',
+    role: 'UI/UX designer',
     image: '/images/team/member-3.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      github: 'https://github.com/oltean-petru',
     }
   },
   {
     id: 4,
-    name: 'John Doe',
-    role: 'Co-founder',
+    name: 'Richard AUGENIE',
+    role: 'UI/UX designer',
     image: '/images/team/member-4.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/richard-augenie-10399a220',
+      instagram: 'https://www.instagram.com/rich_agn/',
     }
   },
   {
     id: 5,
-    name: 'John Doe',
-    role: 'Developer',
+    name: 'Maxence COEUR',
+    role: 'Développeur IOT',
     image: '/images/team/member-5.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/maxence-coeur/',
+      github: 'https://github.com/MAXCOEUR',
     }
   },
   {
     id: 6,
-    name: 'John Doe',
-    role: 'Developer',
+    name: 'Amaury RICHARD DE VESVROTTE',
+    role: 'Développeur IOT',
     image: '/images/team/member-6.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/amaury-richard-de-vesvrotte-1903801b7/',
     }
   },
   {
     id: 7,
-    name: 'John Doe',
-    role: 'Developer',
+    name: 'Sacha BARBET',
+    role: 'Développeur IOT',
     image: '/images/team/member-7.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/sacha-barbet/',
+      github: 'https://github.com/sachabarbet',
     }
   },
   {
     id: 8,
-    name: 'John Doe',
-    role: 'Developer',
+    name: 'Joana BARDONE',
+    role: 'Graphiste/Designer',
     image: '/images/team/member-8.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      instagram: 'https://www.instagram.com/4ksh4a?igsh=b2hrNTRvMjN1NTNn&utm_source=qr',
     }
   },
   {
     id: 9,
-    name: 'John Doe',
-    role: 'Marketing',
+    name: 'Jamil Abo-Alrub',
+    role: 'Data scientist',
     image: '/images/team/member-9.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/jamil-aboalrub',
     }
   },
   {
     id: 10,
-    name: 'John Doe',
-    role: 'Marketing',
+    name: 'Adelia Fathipour',
+    role: 'Développeuse mobile',
     image: '/images/team/member-10.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/adelia-fathipour-040872231',
+      github: 'https://github.com/SajedehAdelia',
     }
   },
   {
     id: 11,
-    name: 'John Doe',
-    role: 'Marketing',
+    name: 'Gaëtan CHOLLET',
+    role: 'Développeur web',
     image: '/images/team/member-11.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/ga%C3%ABtan-chollet-developpeur/',
+      github: 'https://github.com/GaetanccgYnov',
     }
   },
   {
     id: 12,
-    name: 'John Doe',
-    role: 'Marketing',
+    name: 'Thibault RIBES',
+    role: 'Développeur web',
     image: '/images/team/member-12.jpg',
     socials: {
-      linkedin: '#',
-      twitter: '#',
-      instagram: '#',
-      facebook: '#'
+      linkedin: 'https://www.linkedin.com/in/thibault-ribes/',
+      github: 'https://github.com/Zombizz97',
     }
   }
 ]
