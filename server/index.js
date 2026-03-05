a420431f63a9002b628f340c90f92f980179bdef
gaetanccg <gaetan6942@gmail.com>
2026-03-05 16:29:07 +0100

    fix deploy 4

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(distPath));
 
     // SPA fallback
-    app.get('(.*)', (req, res) => {
+    app.get('{*path}', (req, res) => {
         res.sendFile(path.join(distPath, 'index.html'));
     });
 }
