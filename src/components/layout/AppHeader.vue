93b9fa32a4ae50c07b448d3770b0317c9cd65627
ThibaultR <94461612+Zombizz97@users.noreply.github.com>
2026-04-15 09:47:36 +0200

    fix cart color (#64)

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -126,7 +126,7 @@ onUnmounted(() => {
                         class="relative p-2 rounded-full transition-colors"
                         :class="isScrolled ? 'text-gray-700 hover:bg-gray-100' : 'text-white hover:bg-white/10'"
                     >
-                        <img src="/icons/shopping-cart.svg" alt="Panier" class="w-6 h-6 transition-all" :class="{ 'invert': !isScrolled }" />
+                        <img src="/icons/shopping-cart.svg" alt="Panier" class="w-6 h-6 transition-all" :class="{ 'brightness-0 invert': !isScrolled }" />
                         <span
                             v-if="cartCount > 0"
                             class="absolute -top-1 -right-1 w-5 h-5 bg-red-500 text-white text-xs font-bold rounded-full flex items-center justify-center"
@@ -143,7 +143,7 @@ onUnmounted(() => {
                         class="relative p-2 rounded-lg"
                         :class="isScrolled ? 'text-gray-700' : 'text-white'"
                     >
-                        <img src="/icons/shopping-cart.svg" alt="Panier" class="w-6 h-6 transition-all" :class="{ 'invert': !isScrolled }" />
+                        <img src="/icons/shopping-cart.svg" alt="Panier" class="w-6 h-6 transition-all" :class="{ 'brightness-0 invert': !isScrolled }" />
                         <span
                             v-if="cartCount > 0"
                             class="absolute -top-1 -right-1 w-5 h-5 bg-red-500 text-white text-xs font-bold rounded-full flex items-center justify-center"
