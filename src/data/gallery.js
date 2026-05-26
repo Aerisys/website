579414ce07ba570af328a22cebc0d6e80eaf805e
zombizz <thibaultribes.tr@gmail.com>
2026-05-26 21:36:10 +0200

    add img and vid

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -13,20 +13,54 @@ export const galleryImages = [
         id: 3,
         src: '/images/home/mockup_mobile.png',
         alt: 'Mockup mobile de l\'application Aerisys'
+    },
+    {
+        id: 4,
+        src: '/images/home/drone.jpg',
+        alt: 'Drone Aerisys sans hélices'
+    },
+    {
+        id: 5,
+        src: '/images/home/drone_with_propeller.jpg',
+        alt: 'Drone Aerisys avec hélices'
+    },
+    {
+        id: 6,
+        src: '/images/home/landing.png',
+        alt: 'Ecran de démarrage de l\'application Aerisys'
     }
-    // {
-    //     id: 4,
-    //     src: '/images/home/crea.png',
-    //     alt: 'Drone Aerisys composants'
-    // },
-    // {
-    //     id: 5,
-    //     src: '/images/home/crea.png',
-    //     alt: 'Drone Aerisys en action'
-    // },
-    // {
-    //     id: 6,
-    //     src: '/images/home/crea.png',
-    //     alt: 'Drone Aerisys détails'
-    // }
 ];
+
+export const galleryMobiles = [
+    {
+        id: 1,
+        src: '/images/home/landing.png',
+        alt: 'Ecran de démarrage de l\'application Aerisys'
+    },
+    {
+        id: 2,
+        src: 'https://cdn.aerisys.fr/medias/images/mobile_app/homepage.png',
+        alt: 'Page d\'accueil de l\'application Aerisys'
+    },
+    {
+        id: 3,
+        src: 'https://cdn.aerisys.fr/medias/images/mobile_app/login.png',
+        alt: 'Page de connexion de l\'application Aerisys'
+    },
+    {
+        id: 4,
+        src: 'https://cdn.aerisys.fr/medias/images/mobile_app/dronelist.png',
+        alt: 'Index des drones de l\'application Aerisys'
+    },
+    {
+        id: 5,
+        src: 'https://cdn.aerisys.fr/medias/images/mobile_app/dronepage.png',
+        alt: 'Détails d\'un drone sur l\'application Aerisys'
+    },
+    {
+        id: 6,
+        src: 'https://cdn.aerisys.fr/medias/images/mobile_app/widget.png',
+        alt: 'Ajout de widget depuis l\'application Aerisys'
+    }
+];
+
